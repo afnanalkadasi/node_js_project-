7b01fee04441506df32208d313d1212e9f9060f2
afnanalkadasi <afnanalkadasi22@gmail.com>
2022-02-16 11:32:52 +0300

    2 commit

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
-const fileupload = require('express-fileupload');
-const router=express.Router();
+
 
 let initial_path = path.join(__dirname, "public");
 
 const app = express();
 app.use(express.static(initial_path));
-app.use(fileupload());
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "index.html"));
@@ -46,25 +43,6 @@ app.get('/admin', (req, res) => {
 app.get('*', (req, res) => {
 	res.statusCode = 404;
   			res.end(' Sorry, page not found');
-})
-// upload link
-app.post('/upload', (req, res) => {
-    let file = req.files.image;
-    let date = new Date();
-    // image name
-    let imagename = date.getDate() + date.getTime() + file.name;
-    // image upload path
-    let path = 'public/uploads/' + imagename;
-
-    // create upload
-    file.mv(path, (err, result) => {
-        if(err){
-            throw err;
-        } else{
-            // our image upload path
-            res.json(`uploads/${imagename}`)
-        }
-    })
 })
 
 
